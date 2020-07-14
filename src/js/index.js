34fc93580dc6417d67f52de295604255560ec8d4
anluh <andrii.luhvishchyk@smile-ukraine.com>
2020-07-14 15:27:26 +0300

    Scroll behavior

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,17 @@ $(window).scroll(function () {
     $('.main-header__title, .main-header__sub-title, .main-header__menu').slideDown(200);
   }
 });
+
+$(document).ready(function($) {
+  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    e.preventDefault();
+    let target = this.hash,
+      $target = $(target);
+
+    $('html, body').stop().animate( {
+      'scrollTop': $target.offset().top-150
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    } );
+  } );
+} );
